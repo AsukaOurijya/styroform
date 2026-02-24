0961cedace47b6f9acfc7c3ac38a51fc5f7fbe9b
AsukaOurijya <azkaawliya06@gmail.com>
2026-02-24 19:02:33 +0700

    Added login page

--- frontend/src/pages/Login/LoginPage.jsx
+++ frontend/src/pages/Login/LoginPage.jsx
@@ -1,7 +1,34 @@
+import "./LoginStyle.css";
+
 export default function Login() {
     return(
-        <div>
-            <h1>Login</h1>
+        <div className="login-page">
+            <div className="login-container">
+                <h2 className="form-title">Welcome Back!</h2>
+                
+                <p className="separator"><span>Login</span></p>
+
+                <form action="#" className="login-form">
+                    <div className="input-wrapper">
+                        <input type="username" placeholder="Username" className="input-field" required/>
+                        <i className="material-symbols-outlined">account_circle</i>
+                    </div>
+
+                    <div className="input-wrapper">
+                        <input type="password" placeholder="Password" className="input-field" required/>
+                        <i className="material-symbols-outlined">lock</i>
+                    </div>
+
+                    <a href="#" className="forgot-pass-link">Forgot Password?</a>
+
+                    <button className="login-button">
+                        Submit 
+                    </button>
+
+                    <p className="signup-text">No account yet? <a href="#">Create one here!</a></p>
+                </form>
+
+            </div>
         </div>
     );
-}
+} 
