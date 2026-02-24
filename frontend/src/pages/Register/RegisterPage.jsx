be3e5d8244dbea54aa86d9104828538e3adc8f1c
AsukaOurijya <azkaawliya06@gmail.com>
2026-02-24 19:17:30 +0700

    Added register page

--- frontend/src/pages/Register/RegisterPage.jsx
+++ frontend/src/pages/Register/RegisterPage.jsx
@@ -1,7 +1,34 @@
+import "./RegisterStyle.css";
+
 export default function Register() {
     return(
-        <div>
-            <h1>Register</h1>
+        <div className="register-page">
+            <div className="register-container">
+                <h2 className="form-title">Create New Account</h2>
+                
+                <p className="separator"><span>Register</span></p>
+
+                <form action="#" className="register-form">
+                    <div className="input-wrapper">
+                        <input type="username" placeholder="Username" className="input-field" required/>
+                        <i className="material-symbols-outlined">account_circle</i>
+                    </div>
+
+                    <div className="input-wrapper">
+                        <input type="password" placeholder="Password" className="input-field" required/>
+                        <i className="material-symbols-outlined">lock</i>
+                    </div>
+
+                    <a href="#" className="forgot-pass-link">Forgot Password?</a>
+
+                    <button className="register-button">
+                        Submit 
+                    </button>
+
+                    <p className="signup-text">Already have one? <a href="#">Login here!</a></p>
+                </form>
+
+            </div>
         </div>
     );
 }
